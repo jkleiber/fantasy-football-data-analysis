c3ae5c958a265f0aa2d432e243e09f0affb96196
Justin Kleiber <jkleiber8@gmail.com>
2023-12-19 09:45:22 -0500

    Adds argparse and updates for semifinal matchups

--- playoff_mc/playoff_mc.py
+++ playoff_mc/playoff_mc.py
@@ -1,22 +1,37 @@
 
+import argparse
+
 from tournament import Tournament
 
-n_runs = 10000
 
 if __name__ == "__main__":
-   
+    # Parse user arguments
+    parser = argparse.ArgumentParser(
+        prog="Playoff MC",
+        description="Runs a Monte-Carlo for a playoff bracket to determine likelihood of a champion")
+
+    # File defining the playoff structure and teams
+    parser.add_argument('file')
+    # Number of playoffs to simulate.
+    parser.add_argument('--n_runs', default=100000)
+
+    args = parser.parse_args()
+
+    # Turn arguments into local variables.
+    tournament_file = args.file
+    n_runs = args.n_runs
 
     # Save the winners.
     winners = {}
     for i in range(n_runs):
-        tournament = Tournament("data/tournament.json", i)
+        tournament = Tournament(tournament_file, i)
 
         winner = tournament.run_tournament(False, False)
-        
+
         win_name = winner.get_name()
         if win_name in winners.keys():
-            winners[win_name] += 1
+            winners[win_name] += 1 / n_runs
         else:
-            winners[win_name] = 1
-    
+            winners[win_name] = 1 / n_runs
+
     print(winners)
